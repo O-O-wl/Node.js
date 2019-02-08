9942892d6ee55db057be9c94ea52433cfc27990b
dongY <ldcpaul@hanmail.net>
2019-02-08 22:34:41 +0900

    노드 모듈화

--- Chapher03/노드모듈시스템/func.js
+++ Chapher03/노드모듈시스템/func.js
@@ -1,4 +1,15 @@
-const {odd,even} = require('./var')
+const {odd , even} = require('./var'); // 반입구문
 
-console.log("odd")
-console.log('even')
+//console.log("odd");
+//console.log('even');
+
+function checkOddOrEven(num){
+    if( (num%2) == 0){
+        return even
+    }
+    else {
+        return even
+    }
+}
+
+module.exports = checkOddOrEven;
--- Chapher03/노드모듈시스템/var.js
+++ Chapher03/노드모듈시스템/var.js
@@ -1,6 +1,8 @@
-const odd = "홀수"
-const even = "짝수"
+const odd = "홀수";
+const even = "짝수";
 
-module.exports = {
-    odd , even
-}
+// 반출 구문
+ module.exports = {
+    odd,    // odd : odd
+     even   // even : even
+};
