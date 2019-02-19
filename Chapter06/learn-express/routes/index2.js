bb943913a7db7e7a1fd4a76ad20090fc0e0aa292
dongY <ldcpaul@hanmail.net>
2019-02-19 23:32:02 +0900

    pug - include - 공통된 부분  , layout - 공통된 레이아웃 처리

--- Chapter06/learn-express/routes/index2.js
+++ Chapter06/learn-express/routes/index2.js
@@ -6,13 +6,21 @@ router.get('/',(req,res,next)=>{
     console.log('pug')
     res.render('test',{
         title:'Hello',
-        title2:'Express'
+        title2:'Express',
+        fruits:['아보카도','피망','메론']
+
     });
     //res.send('index2');
 });
 
 router.get('/users',(req,res,next)=>{
-    res.send('index2/users');
+ //   res.send('index2/users');
+    res.render('test2',{
+        title:'Hello',
+        title2:'Express',
+        fruits:['아보카도','피망','메론']
+
+    });
 });
 
 
