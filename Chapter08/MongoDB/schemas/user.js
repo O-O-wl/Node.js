e6e6cbe3dc00c2e15412c894133fde92f2626d7c
dongY <ldcpaul@hanmail.net>
2019-02-21 09:43:45 +0900

    스키마 객체 , mongoose 를 이용하여 어느정도 규격 제한

---
+++ Chapter08/MongoDB/schemas/user.js
@@ -0,0 +1,38 @@
+
+
+
+/**========================================================
+
+                @Note - 사용자 스키마
+
+ ===========================================================*/
+
+
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const userSchema = new Schema({
+    name:{
+        type: String, //자바스크립트의 객체
+        required: true, // NOT NULL
+        unique : true,
+    },
+    age:{
+        type: Number,
+        required: true,
+    },
+    married:{
+        type: Boolean,
+        required: true,
+    },
+
+    comment: String, // 설정값이 Type 1개 뿐이라서
+
+    createAt:{
+        type:Date,
+        default: Date.now(), //기본값
+    }
+
+});
+
+module.exports = mongoose.model('User',userSchema);
